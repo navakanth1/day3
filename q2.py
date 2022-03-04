467dbcb26d35e9436f61e5e3274a649d71f5455d
navakanth1 <navakanthreddy4445@gmail.com>
2022-03-04 15:41:48 +0530

    list comprehension

---
+++ q2.py
@@ -0,0 +1,17 @@
+#list=[i for i in range(2, 100) if i%7==0]
+#print(list)
+
+
+
+
+#new_list = [j for j in range(11) if j%2==0]
+#new_list.append(i)
+#print(new_list)
+#for j in new_list:
+
+list = []
+for i in range(1, 100):
+    if i%7==0:
+        list.appeand(i)
+
+print(list)
